5b326b55135149d554e0e5290f654a4809750103
91211rathna <belderathnasree@gmail.com>
2020-08-24 22:20:16 +0530

    sixth commit

---
+++ Day 6/Switch3.java
@@ -0,0 +1,9 @@
+public class Switch3 {
+
+	public static void main(String[] args) {
+		Switch2 Switch = new Switch2(5.0,3.0,"white",false,true,true);
+		System.out.println("switch state:");
+		Switch.display();
+	}
+
+}
